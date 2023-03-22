a8da6a4edee8a59da379b95a5f87e2c8277d899b
Kenneth-Galvez-BSIT3N <57065417+PalindromeRotator@users.noreply.github.com>
2023-03-23 02:10:47 +0800

    Complete navigation of all main pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'capstone_titles_evaluation_system';
+
+export class AppComponent implements OnInit {
+  dashboardActive = '';
+  profileActive = '';
+  titlesActive = '';
+  accountsActive = '';
+  panelActive = '';
+  reportActive = '';
+  contentActive = '';
+  showNavFlag = false;
+  constructor(private router: Router) {
+
+  }
+  ngOnInit(): void {
+
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        if (event.url !== '/' && event.url !== 'register' && event.url !== 'forgot-password') {
+          this.showNavFlag = true;
+          this.dashboardActive = event.url == '/dashboard' ? 'active' : '';
+          this.profileActive = event.url == '/profile' ? 'active' : '';
+          this.titlesActive = event.url == '/titles' ? 'active' : '';
+          this.accountsActive = event.url == '/accounts' ? 'active' : '';
+          this.panelActive = event.url == '/panels' ? 'active' : '';
+          this.reportActive = event.url == '/reports' ? 'active' : '';
+          this.contentActive = event.url == '/contents' ? 'active' : '';
+        }
+
+      }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,56 @@
-import { NgModule } from '@angular/core';
+import { NgModule, OnInit } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes, NavigationEnd, Router } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { ProfileComponent } from './profile/profile.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AccountsComponent } from './accounts/accounts.component';
+import { TitlesComponent } from './titles/titles.component';
+import { PanelsComponent } from './panels/panels.component';
+import { ReportsComponent } from './reports/reports.component';
+import { ContentsComponent } from './contents/contents.component';
+
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'accounts', component: AccountsComponent },
+  { path: 'titles', component: TitlesComponent },
+  { path: 'panels', component: PanelsComponent },
+  { path: 'reports', component: ReportsComponent },
+  { path: 'contents', component: ContentsComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    ProfileComponent,
+    DashboardComponent,
+    AccountsComponent,
+    TitlesComponent,
+    PanelsComponent,
+    ReportsComponent,
+    ContentsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
